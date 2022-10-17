dac7b3d49b616d1ad06be5ffcde8ad7410bd6184
elecinnov <107961683+elecinnov@users.noreply.github.com>
2022-10-17 12:13:03 +1000

    Update Cleanup.py
    will now check the available space on the SD card before removing files

--- Cleanup.py
+++ Cleanup.py
@@ -2,7 +2,7 @@
 
 '''
 This program is wriiten to automatically clean the memory of the RPi so that it does not fill up
-It will remove data that is older than (INSERT TIME) than the current time from the data_backup and data_pad directories. The datatext.txt file will need to be cleared mannualy.
+It will remove data that is older than 60 days than the current time from the data_backup and data_pad directories. The datatext.txt file will need to be cleared mannualy.
 '''
 
 import shutil
@@ -11,9 +11,9 @@ import datetime
 import os
 # delete data older than
 days = 60
-#total, used, free = shutil.disk_usage('/home/pi/data_backup')
-#freeGB = free/1024/1024/1024
-freeGB = 1
+total, used, free = shutil.disk_usage('/home/pi/data_backup')
+freeGB = free/1024/1024/1024
+#freeGB = 1
 if (freeGB<2):
     #files = glob.glob("dummy/*.txt")
     files = glob.glob("/home/pi/data_backup/*.txt")

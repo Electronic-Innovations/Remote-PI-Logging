e186888f48284187562725b4b42d9b9a4f9f0cc6
elecinnov <107961683+elecinnov@users.noreply.github.com>
2022-10-17 12:05:16 +1000

    Create Cleanup.py
    This script when attached to a cronjob will delete backup raw data and padded data from over 60 days ago if the available space of the RPi is less than 2GB

---
+++ Cleanup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+'''
+This program is wriiten to automatically clean the memory of the RPi so that it does not fill up
+It will remove data that is older than (INSERT TIME) than the current time from the data_backup and data_pad directories. The datatext.txt file will need to be cleared mannualy.
+'''
+
+import shutil
+import glob
+import datetime
+import os
+# delete data older than
+days = 60
+#total, used, free = shutil.disk_usage('/home/pi/data_backup')
+#freeGB = free/1024/1024/1024
+freeGB = 1
+if (freeGB<2):
+    #files = glob.glob("dummy/*.txt")
+    files = glob.glob("/home/pi/data_backup/*.txt")
+    now = datetime.datetime.now()
+    for file in files:
+        date_string = file.replace('.txt','')
+        #date_string = date_string.replace('dummy/','')
+        date_string = date_string.replace('/home/pi/data_backup','')
+        file_date = datetime.datetime.strptime(date_string,'%Y-%m-%d__%H_%M')
+        difference = now - file_date
+        if(difference> datetime.timedelta(days = days)):
+            if os.path.isfile(file):
+                print("Removing:" + file)
+                os.remove(file)
+            else:    ## Show an error ##
+                print("Error: %s file not found" % file)
+    #files2 = glob.glob("dummy/*.txt")
+    files2 = glob.glob("/home/pi/data_pad/*.txt")
+    for file2 in files2:
+        date_string = file2.replace('pad.txt','')
+        #date_string = date_string.replace('dummy/','')
+        date_string = date_string.replace('/home/pi/data_pad','')
+        file_date = datetime.datetime.strptime(date_string,'%Y-%m-%d__%H_%M')
+        difference = now - file_date
+        if(difference> datetime.timedelta(days = days)):
+            if os.path.isfile(file2):
+                print("Removing:" + file2)
+                os.remove(file2)
+            else:    ## Show an error ##
+                print("Error: %s file not found" % file2)
+    
+    

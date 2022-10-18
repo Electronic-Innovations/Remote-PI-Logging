e40e20b9c2f579f0e6326d50fa3ed7f195c592dc
elecinnov <107961683+elecinnov@users.noreply.github.com>
2022-10-18 11:20:24 +1000

    Changed Cleanup.py functionality

--- Cleanup.py
+++ Cleanup.py
@@ -9,53 +9,51 @@ import shutil
 import glob
 import datetime
 import os
-# remove data older than
-days = 60
 
 print((datetime.datetime.now()).strftime("%Y-%m-%d__%H_%M"))
 #Get the amount of free space in the filesystem
 total, used, free = shutil.disk_usage('/home/pi/data_backup')
 freeGB = free/1024/1024/1024
-print(str(freeGB) + " Gb of Free Space")
 #freeGB = 1
+print(str(freeGB) + " Gb of Free Space")
 #if less than 2GB do a clean up
-if (freeGB<2):
-    print("Less than 2Gb removing files oflder than "+str(days)+" days")
-    #Aquire all the files in the data_backup directory
-    #files = glob.glob("dummy/*.txt")
+if (free/1024/1024/1024<(free/1024/1024/1024)+1):
+    print("Less than 2Gb removing files to ensure 2Gb of space")
+    #Aquire all the files in the data_backup and data_pad directory and sort they alphabetically
+    #files = sorted(glob.glob("dummy/*.txt"))
+    #files2 = sorted(glob.glob("dummypad/*.txt"))
     files = glob.glob("/home/pi/data_backup/*.txt")
+    files2 = glob.glob("/home/pi/data_pad/*.txt")
     #Get current time
     now = datetime.datetime.now()
     # Loop over each file and strip path and .txt, check the filename for datetime of creation
-    for file in files:
-        date_string = file.replace('.txt','')
-        #date_string = date_string.replace('dummy/','')
-        date_string = date_string.replace('/home/pi/data_backup/','')
-        file_date = datetime.datetime.strptime(date_string,'%Y-%m-%d__%H_%M')
-        difference = now - file_date
-        # Compare to see if the file is old enough to be deleted if so delete file
-        if(difference> datetime.timedelta(days = days)):
-            if os.path.isfile(file):
-                print("Removing:" + file)
-                os.remove(file)
-            else:    ## Show an error ##
-                print("Error: file not found:" + file)
-    
-    # Same process but in the data_pad directory
-    #files2 = glob.glob("dummy/*.txt")
-    files2 = glob.glob("/home/pi/data_pad/*.txt")
-    for file2 in files2:
-        date_string = file2.replace('pad.txt','')
-        #date_string = date_string.replace('dummy/','')
-        date_string = date_string.replace('/home/pi/data_pad/','')
-        file_date = datetime.datetime.strptime(date_string,'%Y-%m-%d__%H_%M')
-        difference = now - file_date
-        if(difference> datetime.timedelta(days = days)):
-            if os.path.isfile(file2):
-                print("Removing:" + file2)
-                os.remove(file2)
-            else:    ## Show an error ##
-                print("Error: file not found" + file2)
-else:
-    print("Over 2Gb of space remaning not removing files")
-    
+    if len(files)>= len(files2):
+        num = len(files)
+    else:
+        num = len(files2)
+    for i in range(num):
+        # remove oldest backup file
+        if os.path.isfile(files[i]):
+            print("Removing:" + files[i])
+            os.remove(files[i])
+            #freeGB = freeGB +0.5
+        else:    ## Show an error ##
+            print("Error: file not found:" + files[i])
+        total, used, free = shutil.disk_usage('/home/pi/data_backup')
+        freeGB = free/1024/1024/1024
+        if free/1024/1024/1024<(free/1024/1024/1024)+1:
+            break
+        
+        # remove oldest padfile
+        if os.path.isfile(files2[i]):
+            print("Removing:" + files2[i])
+            os.remove(files2[i])
+            #freeGB = freeGB +0.5
+        else:    ## Show an error ##
+            print("Error: file not found:" + files2[i])
+        total, used, free = shutil.disk_usage('/home/pi/data_backup')
+        freeGB = free/1024/1024/1024
+        if free/1024/1024/1024<(free/1024/1024/1024)+1:
+            break
+quit()
+
